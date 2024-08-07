dff287b02fd1caf1c8de37995ceda276674fcce9
sungjin12345 <gjtjdwls1002@uos.ac.kr>
2024-08-07 18:07:45 +0900

    no message

--- PWM_2.c
+++ PWM_2.c
@@ -16,12 +16,14 @@ void init(double freq, double duty)
 	PWM[2] = 1;
 }
 
-void generatingPWM(double Ts, double freq, double duty, double *Tri, double duty_ref[], double PWM[])
+void generatingPWM(double Ts, double freq, double duty, double deadtime, double *Tri, double duty_ref[], double PWM[])
 {
 
 	T = 1/freq;
 	duty_ref[0] = duty/100;
-	duty_ref[1] = (-duty/100) ;
+	duty_ref[1] = (-duty/100);
+
+	double delta_h = 1.1 * deadtime / T; //Phase change of triangular wave according to dead time setting
 
 	//If the triangluar wave needs to be updated (at the start of a new cycle)
 	if (t >= T)
@@ -31,37 +33,37 @@ void generatingPWM(double Ts, double freq, double duty, double *Tri, double duty
 	//Generate the triangular wave over the entire period
 	if (t < T/2)
 	{
-		*Tri = 1.1 * (2/T)*t - 0.55; //0에서 0.55까지
+		*Tri = 1.1 * (2/T)*t - 0.55; 
 	} else 
 	{
-		*Tri = 1.1 * (2/T)*(T-t) - 0.55; //
+		*Tri = 1.1 * (2/T)*(T-t) - 0.55; 
 	}
-	//Determine the first PWM signals based on the triangular wave (+0.01 for the deadtime) and a sine wave
-	if (*Tri + 0.01 < duty_ref[0])
+	//Determine the first PWM signals based on the triangular wave (incremented for the deadtime) and a sine wave
+	if (*Tri + delta_h < duty_ref[0])
 	{
 		PWM[0] = 1;
 	} else
 	{
 		PWM[0] = 0;
 	}
-	//Determine the first PWM signals based on the triangular wave (-0.01 for the deadtime) and inverted sine wave
-	if (*Tri - 0.01 < duty_ref[0])
+	//Determine the first PWM signals based on the triangular wave (decremented for the deadtime) and inverted sine wave
+	if (*Tri - delta_h < duty_ref[0])
 	{
 		PWM[2] = 0;
 	} else
 	{
 		PWM[2] = 1;
 	}
-	//Determine the second PWM signals based on the triangular wave (+0.01 for the deadtime) and a sine wave
-	if (*Tri + 0.01 < duty_ref[1])
+	//Determine the second PWM signals based on the triangular wave (incremented for the deadtime) and a sine wave
+	if (*Tri + delta_h < duty_ref[1])
 	{
 		PWM[1] = 1;
 	} else
 	{
 		PWM[1] = 0;
 	}
-	//Determine the second PWM signals based on the triangular wave (-0.01 for the deadtime) and inverted sine wave
-	if (*Tri - 0.01 < duty_ref[1])
+	//Determine the second PWM signals based on the triangular wave (decremented for the deadtime) and inverted sine wave
+	if (*Tri - delta_h < duty_ref[1])
 	{
 		PWM[3] = 0;
 	} else
